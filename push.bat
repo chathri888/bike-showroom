5384b9c36c85e3b52c1a344e0a32462d37a27e42
chathri888 <chathri0903@gmail.com>
2026-02-24 18:02:23 +0530

    Delete push.bat

--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set GIT="C:\Users\DELL\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd\git.exe"
-%GIT% add -A
-%GIT% commit -m "Add-hero-slideshow-and-fix-bike-images"
-%GIT% push origin main
