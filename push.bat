a766b9eefea6078918b5328bdb98c780abdf3bad
chathri888 <chathri0903@gmail.com>
2026-02-24 12:09:59 +0530

    commit

--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set GIT="C:\Users\DELL\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd\git.exe"
-%GIT% add -A
-%GIT% commit -m "Fix Jenkinsfile: add Start Minikube stage, fix bash eval"
-%GIT% push origin main
