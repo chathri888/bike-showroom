d279240d19daae77f1ae65d300f24d20d756315f
chathri888 <chathri0903@gmail.com>
2026-02-24 16:27:52 +0530

    Fix-Jenkinsfile-docker-driver

--- push.bat
+++ push.bat
@@ -1,5 +1,6 @@
 @echo off
 set GIT="C:\Users\DELL\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd\git.exe"
+%GIT% pull origin main --allow-unrelated-histories --no-edit
 %GIT% add -A
-%GIT% commit -m "Fix: use docker driver, not none driver for Minikube"
+%GIT% commit -m "Fix-Jenkinsfile-docker-driver" --allow-empty
 %GIT% push origin main
