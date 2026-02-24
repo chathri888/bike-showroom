b388af754957e3f19d8fb95bf9ffc6b2d2404da7
chathri888 <chathri0903@gmail.com>
2026-02-24 17:50:05 +0530

    Delete push.bat

--- push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set GIT="C:\Users\DELL\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd\git.exe"
-%GIT% add -A
-%GIT% commit -m "Update-Classic350-and-PulsarNS200-images"
-%GIT% push origin main
