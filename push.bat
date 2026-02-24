a4248db8b5f0e30fdf3b610ca036aa861f30862d
chathri888 <chathri0903@gmail.com>
2026-02-24 16:29:11 +0530

    Fix-merge-conflicts-in-Jenkinsfile

--- push.bat
+++ push.bat
@@ -1,7 +1,5 @@
 @echo off
 set GIT="C:\Users\DELL\AppData\Local\GitHubDesktop\app-3.5.4\resources\app\git\cmd\git.exe"
-%GIT% config pull.rebase false
-%GIT% pull origin main --no-edit
 %GIT% add -A
-%GIT% commit -m "Fix-Jenkinsfile-docker-driver" --allow-empty
+%GIT% commit -m "Fix-merge-conflicts-in-Jenkinsfile"
 %GIT% push origin main
